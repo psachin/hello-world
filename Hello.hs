1032e47c831c72f143c67abfbab980f03826ad91
Sachin Patil <psachin@redhat.com>
2017-12-30 23:11:15 +0530

    Function taking a function
    This can be done in Python as well

--- Hello.hs
+++ Hello.hs
@@ -1,5 +1,8 @@
 addIntegers :: Integer -> Integer -> Integer
 addIntegers x y = x + y
 
+functionTakingAFunction :: (Integer -> Integer -> Integer) -> Integer -> Integer -> Integer
+functionTakingAFunction f a b = f a b
+
 main :: IO ()
-main = print (addIntegers 7 8)
+main = print (functionTakingAFunction addIntegers 7 8)
